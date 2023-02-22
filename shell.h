c06b42f7ac2dc26c9cad23a30738a8cbe0f4b2b6
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-02-22 11:25:13 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,67 +1,34 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <stddef.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
-#include <signal.h>
+#include <string.h>
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
-char *_strdup(char *str);
-char *concat_all(char *name, char *sep, char *value);
-
-char **splitstring(char *str, const char *delim);
-void execute(char **argv);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+#define strtok_delim " \n\r\a\t"
 
 extern char **environ;
-
-/**
- * struct list_path - Linked list containing PATH directories
- * @dir: directory in path
- * @p: pointer to next node
- */
-typedef struct list_path
-{
-	char *dir;
-	struct list_path *p;
-} list_path;
-
-
-char *_getenv(const char *name);
-list_path *add_node_end(list_path **head, char *str);
-list_path *linkpath(char *path);
-char *_which(char *filename, list_path *head);
-
-/**
- * struct mybuild - pointer to function with corresponding buildin command
- * @name: buildin command
- * @func: execute the buildin command
- */
-typedef struct mybuild
-{
-	char *name;
-	void (*func)(char **);
-} mybuild;
-
-void(*checkbuild(char **arv))(char **arv);
-int _atoi(char *s);
-void exitt(char **arv);
-void env(char **arv);
-void _setenv(char **arv);
-void _unsetenv(char **arv);
-
-void freearv(char **arv);
-void free_list(list_path *head);
-
+int str_to_array(char *cmd_line, int count, char **argv);
+int _exec(char **cmd_list, int i, char *cmd_line, int count, char **argv);
+void command_not_found(int i, char **cmd_list, int count, char **argv);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+char *_strdup(char *str);
+char *_path(char *command);
+char *directory(char *temporal_dir, char *command);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void a_exit(char **text, int i, char *cmd_line, int exit_status);
+void _env(void);
+void signal_handler(int signal);
+int _putchar(char c);
+void print_number(int n);
 
 #endif
